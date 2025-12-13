0dfe9db9b4bb7f2567a138eb5969100f9ffb4e8e
Ratnakar Singh Parihar <ratnakarsinghparihar9399@gmail.com>
2025-12-13 22:15:30 +0530

    fix vercel react routers again 404

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,31 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./styles/tailwind.css";
+// import "./styles/index.css";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+
+// root.render(<App />);
+
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// import "./index.css"; // merge all css here
 import "./styles/tailwind.css";
 import "./styles/index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 
-root.render(<App />);
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
