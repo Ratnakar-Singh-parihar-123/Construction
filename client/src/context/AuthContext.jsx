d1dabea32621c973ba3c5c7d23c4776aaca0a979
Ratnakar Singh Parihar <ratnakarsinghparihar9399@gmail.com>
2025-12-17 23:44:35 +0530

    auth to auths

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext, useEffect } from 'react';
-import { api } from '../pages/auth/Auth';
+import { api } from '../pages/auths/Auth';
 
 const AuthContext = createContext({});
 
